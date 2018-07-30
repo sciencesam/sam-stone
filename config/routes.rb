423a63213e5ceb4ea6b3df0385d3b30d7ba8f806
Sam Stone <sciencesam13@gmail.com>
2018-07-30 03:33:24 +0000

    only admins can post, fixed index spacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, controllers: { registrations: "registrations"}
   #resources :users, :only => [:show]
   mount Ckeditor::Engine => '/ckeditor'
   #get 'static_pages/home'
